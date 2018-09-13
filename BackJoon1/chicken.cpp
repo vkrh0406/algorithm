68d5174d3b82f6fc48368dab0781cd00413a43a6
vkrh0406 <vkrh0406@gmail.com>
2018-09-13 17:43:10 +0900

    chicken fail

--- BackJoon1/chicken.cpp
+++ BackJoon1/chicken.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
 
+// 서울대 프로그래밍 13119번 문제 https://www.acmicpc.net/problem/13199
+
+using namespace std;
 int main()
 {
+	int n;
+
+	cin >> n;
+
+	for (int i = 0;i < n;i++)
+	{
+		int price, money, needCoopon, serviceCoopon;
+		cin >> price >> money >> needCoopon >> serviceCoopon;
+
+		int chicken = money / price;
+		int hCoopon = chicken*serviceCoopon;
+		int moreChick = hCoopon / needCoopon;
+		hCoopon = hCoopon - (moreChick*needCoopon);
+		hCoopon += moreChick*serviceCoopon;
+		int moreChick2 =  hCoopon / needCoopon;
+		cout << moreChick2 << endl;
+
+
+	}
+
+
 
 	return 0;
 
